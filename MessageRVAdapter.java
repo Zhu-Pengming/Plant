c2943be8f9c009e98063ac22f6f9a09a42db3993
Tom <89885722+Zhu-Pengming@users.noreply.github.com>
2024-05-13 09:11:26 +0800

    Update MessageRVAdapter.java

--- MessageRVAdapter.java
+++ MessageRVAdapter.java
@@ -1,23 +1,26 @@
 package com.example.npm;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import java.io.InputStream;
 import java.util.ArrayList;
 
-public class MessageRVAdapter extends RecyclerView.Adapter {
+public class MessageRVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    // variable for our array list and context.
     private ArrayList<Message> messageModalArrayList;
     private Context context;
 
-    // constructor class.
     public MessageRVAdapter(ArrayList<Message> messageModalArrayList, Context context) {
         this.messageModalArrayList = messageModalArrayList;
         this.context = context;
@@ -27,15 +30,11 @@ public class MessageRVAdapter extends RecyclerView.Adapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
-        // below code is to switch our
-        // layout type along with view holder.
         switch (viewType) {
             case 0:
-                // below line we are inflating user message layout.
                 view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_item, parent, false);
                 return new UserViewHolder(view);
             case 1:
-                // below line we are inflating bot message layout.
                 view = LayoutInflater.from(parent.getContext()).inflate(R.layout.bot_item, parent, false);
                 return new BotViewHolder(view);
         }
@@ -44,33 +43,35 @@ public class MessageRVAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        // this method is use to set data to our layout file.
-        Message modal = messageModalArrayList.get(position);
-        switch (modal.getSender()) {
+        Message message = messageModalArrayList.get(position);
+        switch (message.getSender()) {
             case "user":
-                // below line is to set the text to our text view of user layout
-                ((UserViewHolder) holder).userTV.setText(modal.getMessage());
+                UserViewHolder userHolder = (UserViewHolder) holder;
+                userHolder.userTV.setText(message.getMessage());
+                if (message.getImageUri() != null) {
+                    Bitmap image = loadImageFromUri(Uri.parse(message.getImageUri()));
+                    userHolder.userIV.setImageBitmap(image);
+                }
                 break;
             case "bot":
-            {
-                // below line is to set the text to our text view of bot layout
-                ((BotViewHolder) holder).botTV.setText(Html.fromHtml(modal.getMessage()));
-                ((BotViewHolder) holder).botTV.setMovementMethod(LinkMovementMethod.getInstance());
+                BotViewHolder botHolder = (BotViewHolder) holder;
+                botHolder.botTV.setText(Html.fromHtml(message.getMessage()));
+                botHolder.botTV.setMovementMethod(LinkMovementMethod.getInstance());
+                if (message.getImageUri() != null) {
+                    Bitmap image = loadImageFromUri(Uri.parse(message.getImageUri()));
+                    botHolder.botIV.setImageBitmap(image);
+                }
                 break;
-            }
-
         }
     }
 
     @Override
     public int getItemCount() {
-        // return the size of array list
         return messageModalArrayList.size();
     }
 
     @Override
     public int getItemViewType(int position) {
-        // below line of code is to set position.
         switch (messageModalArrayList.get(position).getSender()) {
             case "user":
                 return 0;
@@ -81,29 +82,34 @@ public class MessageRVAdapter extends RecyclerView.Adapter {
         }
     }
 
-    public static class UserViewHolder extends RecyclerView.ViewHolder {
+    private Bitmap loadImageFromUri(Uri uri) {
+        try (InputStream inputStream = context.getContentResolver().openInputStream(uri)) {
+            return BitmapFactory.decodeStream(inputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-        // creating a variable
-        // for our text view.
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
         TextView userTV;
+        ImageView userIV;
 
         public UserViewHolder(@NonNull View itemView) {
             super(itemView);
-            // initializing with id.
             userTV = itemView.findViewById(R.id.idTVUser);
+            userIV = itemView.findViewById(R.id.idIVUser);  // Assuming you have an ImageView in your layout
         }
     }
 
     public static class BotViewHolder extends RecyclerView.ViewHolder {
-
-        // creating a variable
-        // for our text view.
         TextView botTV;
+        ImageView botIV;
 
         public BotViewHolder(@NonNull View itemView) {
             super(itemView);
-            // initializing with id.
             botTV = itemView.findViewById(R.id.idTVBot);
+            botIV = itemView.findViewById(R.id.idIVBot);  // Assuming you have an ImageView in your layout
         }
     }
 }
