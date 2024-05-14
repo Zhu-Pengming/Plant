2c4ac7d896e083523c68e7709580bb552508ac86
Tom <89885722+Zhu-Pengming@users.noreply.github.com>
2024-05-14 14:16:50 +0800

    Add files via upload

---
+++ com/example/npm/ChatSession.java
@@ -0,0 +1,52 @@
+package com.example.npm;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@Entity(tableName = "chat_sessions")
+public class ChatSession {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "timestamp")
+    private long timestamp;
+
+    public ChatSession(String name,  long timestamp) {
+        this.name = name;
+        this.timestamp = timestamp;
+    }
+
+    // Getter 和 Setter 方法
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getFormattedTimestamp() {
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm a", Locale.getDefault());
+        return formatter.format(new Date(this.timestamp)); }
+}
