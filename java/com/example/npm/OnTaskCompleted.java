e15491a34f02d49961dd4c5294907f956867f1ee
Tom <89885722+Zhu-Pengming@users.noreply.github.com>
2024-06-10 14:49:25 +0800

    Delete java/com/example/npm directory

--- java/com/example/npm/OnTaskCompleted.java
+++
@@ -1,8 +0,0 @@
-package com.example.npm;
-
-import org.json.JSONException;
-
-public interface OnTaskCompleted {
-    void onTaskCompleted(String result) throws JSONException;
-    void onFailure(Exception e);
-}
